7b209f3be506a6f0d81f6a881c772db77154a6a7
PGimenez <gimenezfebrer@gmail.com>
2024-01-24 18:38:26 +0100

    time elapsed

--- app.jl
+++ app.jl
@@ -16,10 +16,34 @@ symbols = [
     "CF", "CFG", "CHD", "CHRW", "CHTR", "CI", "CINF", "CL", "CLX", "CMA"
 ]
 
-# add reactive code to make the UI interactive
+using Dates
+
+function time_elapsed(num_days::Int)
+    current_time = now()
+    past_time = current_time - Day(num_days)
+
+    delta_days = Dates.value(Dates.Day(current_time - past_time))
+    years, remaining_days = divrem(delta_days, 365)
+    months, days = divrem(remaining_days, 30)
+
+    elapsed_time = String[]
+    if years > 0
+        push!(elapsed_time, "$(years) year$(years > 1 ? "s" : "")")
+    end
+    if months > 0
+        push!(elapsed_time, "$(months) month$(months > 1 ? "s" : "")")
+    end
+    if days > 0
+        push!(elapsed_time, "$(days) day$(days > 1 ? "s" : "")")
+    end
+
+    return join(elapsed_time, ", ")
+end
+
 @app begin
     @in start_date = "2021-01-01"
     @in end_date = "2022-01-01"
+    @out elapsed = "1 year"
     @out stocks = symbols
     @in selected_stock = "AAPL"
     @out prices = DataFrame(ticker=[], close=[], timestamp=[], ma20=[])
@@ -40,17 +64,17 @@ symbols = [
         percent_return = round((prices[!,:close][end] - prices[!,:close][1]) / prices[!,:close][1] * 100, digits=3)
         avgval = round(mean(prices[!,:close]), digits=3)
         news = get_news([selected_stock])[1] |> convert_to_serializable
+    elapsed = time_elapsed(Dates.value(Dates.Day(Date(end_date)-Date(start_date))))
+    @show elapsed, Dates.value(Dates.Day(Date(end_date)-Date(start_date)))
     end
     @onchange window begin
         start_date[!] = today() - Day(window) |> string # update variable without triggering the handler
         @push start_date
         end_date = today() |> string
+        elapsed = time_elapsed(window)
     end
 end
 
-
-# register a new route and the page that will be
-# loaded on access
 @page("/", "app.jl.html")
 end
 
